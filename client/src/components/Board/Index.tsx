a5dda12d110280a0e840feb18ce3740dae244579
Yashvardhan Arora <yash22arora@gmail.com>
2022-09-02 19:43:41 +0530

    Install DnD npm package
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/components/Board/Index.tsx
+++ client/src/components/Board/Index.tsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import { BsPlus } from "react-icons/bs";
 import MockData from "./../../MOCK_DATA.json";
 import Section from "./Section";
 const Board: React.FC = () => {
+  const [data, setData] = useState(MockData);
   return (
     <div className="flex flex-row h-full w-full p-4 pt-8">
       <div className="flex min-w-full h-full overflow-scroll">
-        {MockData.map((section) => {
+        {data.map((section) => {
           return <Section key={section.id} section={section} />;
         })}
-        <BsPlus size={100} />
+        <BsPlus size={100} className="cursor-pointer" />
       </div>
     </div>
   );
--- client/src/components/Board/Task.tsx
+++ client/src/components/Board/Task.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BsTextParagraph } from "react-icons/bs";
 
 interface TaskProps {
@@ -10,11 +11,26 @@ interface TaskProps {
   };
 }
 const Task: React.FC<TaskProps> = ({ task }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [height, setHeight] = useState("h-0");
+  useEffect(() => {
+    setHeight(expanded ? "h-min my-3" : "h-0");
+  }, [expanded]);
   return (
     <div className="flex flex-col p-4 rounded-md bg-slate-500 shadow-md my-3">
-      <span className="text-lg font-medium">{task.title}</span>
+      <span className="text-xl font-bold">{task.title}</span>
+      <p
+        className={`${height} transition-all origin-top duration-200 overflow-hidden`}
+      >
+        {task.description}
+      </p>
       <div className="flex flex-row">
-        <BsTextParagraph />
+        <BsTextParagraph
+          className="cursor-pointer mt-3"
+          onClick={() => {
+            setExpanded((prev) => !prev);
+          }}
+        />
       </div>
     </div>
   );
